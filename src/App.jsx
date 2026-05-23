bda5be13f429c58e2dd148a02c54c527527a637d
Adit Kewate <aditkewate@example.com>
2026-05-23 15:08:39 +0530

    Removed the Resume section from the page

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Skills />
         <Projects />
         {/* <Experience /> */}
-        <Resume />
+        {/* <Resume /> */}
         <Contact />
       </main>
     </>
